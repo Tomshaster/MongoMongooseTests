5ffb4d48ef748d9f94a93e1150c61ee71e30d1f0
Tomshaster <tomascartagena00@gmail.com>
2023-03-09 19:19:58 -0300

    added env var

--- myApp.js
+++ myApp.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
-const MONGO_URI = require("./");
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  "mongodb+srv://Admin:<Admin>@clusterprime.oybpsnq.mongodb.net/fcc-mongodbtest?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true }
-);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 let Person;
 
